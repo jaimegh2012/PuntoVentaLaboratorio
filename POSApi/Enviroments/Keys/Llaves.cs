19953045a1a74629a165a5746acb6fcb4abe5a68
Jaime Gutierrez <jaimegh_2012@hotmail.com>
2024-07-18 14:27:54 -0600

    Servicio encriptacion AES

---
+++ POSApi/Enviroments/Keys/Llaves.cs
@@ -0,0 +1,18 @@
+﻿namespace POSApi.Enviroments.Keys
+{
+    public class Llaves
+    {
+        // Cifrado AES (tienen que ser las mismas de backoffice)
+        //public readonly string AES_KEYs = _configuration.GetValue<string>("AesKeys:AES_KEY");
+        public readonly string AES_KEY = new ConfigurationBuilder()
+                    .SetBasePath(Directory.GetCurrentDirectory())
+                    .AddJsonFile("appsettings.json")
+                    .Build().GetValue<string>("AesKeys:AES_KEY");
+
+
+        public readonly string AES_VECTOR_INITIAL = new ConfigurationBuilder()
+                    .SetBasePath(Directory.GetCurrentDirectory())
+                    .AddJsonFile("appsettings.json")
+                    .Build().GetValue<string>("AesKeys:AES_VECTOR_INITIAL");
+    }
+}
---
+++ POSApi/Security/AES/AES.cs
@@ -0,0 +1,74 @@
+﻿
+
+using POSApi.Enviroments.Keys;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace POSApi.Security.AES
+{
+    public class AES
+    {
+        public static string EncryptString(string plainText)
+        {
+            Llaves keys = new();
+
+            byte[] array;
+            var iv = Encoding.UTF8.GetBytes(keys.AES_VECTOR_INITIAL);
+
+            using (Aes aes = Aes.Create())
+            {
+                aes.Key = Encoding.UTF8.GetBytes(keys.AES_KEY);
+                aes.IV = iv;
+                aes.Padding = PaddingMode.PKCS7;
+                aes.Mode = CipherMode.CBC;
+
+                ICryptoTransform encryptor = aes.CreateEncryptor(aes.Key, aes.IV);
+
+                using (MemoryStream memoryStream = new MemoryStream())
+                {
+                    using (CryptoStream cryptoStream = new CryptoStream((Stream)memoryStream, encryptor, CryptoStreamMode.Write))
+                    {
+                        using (StreamWriter streamWriter = new StreamWriter((Stream)cryptoStream))
+                        {
+                            streamWriter.Write(plainText);
+                        }
+
+                        array = memoryStream.ToArray();
+                    }
+                }
+            }
+
+            return Convert.ToBase64String(array);
+        }
+
+        public static string DecryptString(string cipherText)
+        {
+            Llaves keys = new();
+
+            var cipherTextReformado = cipherText.Replace(" ", "+");
+
+            var iv = Encoding.UTF8.GetBytes(keys.AES_VECTOR_INITIAL);
+
+            byte[] buffer = Convert.FromBase64String(cipherTextReformado);
+            using (Aes aes = Aes.Create())
+            {
+                aes.Key = Encoding.UTF8.GetBytes(keys.AES_KEY);
+                aes.IV = iv;
+                ICryptoTransform decryptor = aes.CreateDecryptor(aes.Key, aes.IV);
+                using (MemoryStream memoryStream = new MemoryStream(buffer))
+                {
+                    using (CryptoStream cryptoStream = new CryptoStream((Stream)memoryStream, decryptor, CryptoStreamMode.Read))
+                    {
+                        using (StreamReader streamReader = new StreamReader((Stream)cryptoStream))
+                        {
+                            return streamReader.ReadToEnd();
+                        }
+                    }
+                }
+            }
+        }
+
+
+
+    }
+}
